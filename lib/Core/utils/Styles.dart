8e14981e38a3807a870c028f94eb2e980ca0a2f3
islam <islamashraf520@gmail.com>
2023-11-21 22:13:02 +0200

    add font

--- lib/Core/utils/Styles.dart
+++ lib/Core/utils/Styles.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 abstract class Styles {
   static const titleMediam = TextStyle(
     fontSize: 23,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w800,
     color: Colors.black,
   );
 }
