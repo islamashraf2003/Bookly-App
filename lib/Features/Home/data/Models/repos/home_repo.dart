c417e2b41450a2a944d90af1e17db8f3bf7b71c0
islam <islamashraf520@gmail.com>
2024-01-14 17:03:56 +0200

    using darz package (Either return type)

--- lib/Features/Home/data/Models/repos/home_repo.dart
+++ lib/Features/Home/data/Models/repos/home_repo.dart
@@ -1,4 +1,13 @@
+import 'package:booky_app/Core/Errors/Failure.dart';
+import 'package:booky_app/Features/Home/data/Models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFavBooks();
-  fetchFeautureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFavBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeautureBooks();
 }
+/*
+Either exist in darz package have tow parametars <left , right>
+hava expexted tow return type .
+
+*/
