d0ac55d18f0c9c151d1f3c083f08b4de79c9d31e
islam <islamashraf520@gmail.com>
2024-01-20 14:33:36 +0200

    add implementation to featured books cubit

--- lib/Features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/Home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,22 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:booky_app/Features/Home/data/Models/book_model/book_model.dart';
+import 'package:booky_app/Features/Home/data/Models/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeautureBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(errorMessage: failure.errorMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books: books));
+    });
+  }
 }
